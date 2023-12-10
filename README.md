3374ed94302c9397390fb470aea24dde824c08cc
ImproperIssues <ItziSpyder@gmail.com>
2023-12-10 03:09:14 -0800

    update README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Plugin Template for Paper
+Template uses PDK (ItziSpyder/PDK).
+
+**This Plugin uses PDK https://github.com/itzispyder/PDK**
--- src/main/java/io/github/itzispyder/plugintemplate/events/ExampleListener.java
+++ src/main/java/io/github/itzispyder/plugintemplate/events/ExampleListener.java
@@ -1,12 +1,11 @@
 package io.github.itzispyder.plugintemplate.events;
 
-import io.github.itzispyder.pdk.Global;
 import io.github.itzispyder.pdk.events.CustomListener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class ExampleListener implements CustomListener, Global {
+public class ExampleListener implements CustomListener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
